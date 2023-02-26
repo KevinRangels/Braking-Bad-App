6b3bf024aed57435a6f6816a39ab56e0876891f4
Kevin Rangel <krangel107@gmail.com>
2023-02-26 13:50:33 -0500

    Global state with composables functions

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
   import { ref } from 'vue'
-  import rickAndMortyApi from '@/api/breakingBadApi'
+  import rickAndMortyApi from '@/api/rickAndMortyApi'
   import type { Character, Result } from '@/characters/interfaces/character'
+  import { useCharacters } from '@/characters/composables/useCharacters'
 
   // const { data } = await rickAndMortyApi.get<Character>('/character')
   // const characters = data.results
-  const characters = ref<Result[]>([])
 
-  rickAndMortyApi.get<Character>('/character').then(resp => {
-    console.log('resp', {data: resp.data})
-    characters.value = resp.data.results
-  })
+  const {isLoading, charactersm, hasError, errorMessage} = useCharacters()
+  
 </script>
 
 <template>
+  <h1 v-if="isLoading">
+    Loading
+  </h1>
+  <h1 v-if="hasError">
+    {{ errorMessage }}
+  </h1>
   <ul>
     <li
       v-for="{id, name} of characters"
